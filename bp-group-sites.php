a7a50c789e8f20a948331a9e2a7aca9f4678b7a5
Christian Wach <needle@haystack.co.uk>
2016-02-02 12:33:33 +0000

    bump version to 0.2.1

--- bp-group-sites.php
+++ bp-group-sites.php
@@ -3,7 +3,7 @@
 --------------------------------------------------------------------------------
 Plugin Name: BP Group Sites
 Description: In WordPress Multisite, create a many-to-many replationship between BuddyPress Groups and WordPress Sites.
-Version: 0.2
+Version: 0.2.1
 Author: Christian Wach
 Author URI: http://haystack.co.uk
 Plugin URI: https://github.com/christianwach/bp-group-sites
@@ -14,7 +14,7 @@ Network: true
 
 
 // set our version here
-define( 'BPGSITES_VERSION', '0.2' );
+define( 'BPGSITES_VERSION', '0.2.1' );
 
 // store reference to this file
 if ( ! defined( 'BPGSITES_FILE' ) ) {
